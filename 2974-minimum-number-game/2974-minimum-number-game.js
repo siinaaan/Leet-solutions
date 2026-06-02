448c5b1372a6807f4dc652fd494b82be386d6c29
Muhammed Sinan <siinaaan21@gmail.com>
2026-06-02 12:10:12 +0530

    Time: 2 ms (82.35%), Space: 58.2 MB (31.02%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var numberGame = function(nums) {
+    nums.sort((a,b)=>a-b);
+
+    let arr = [];
+
+    for(let i=0;i<nums.length;i=i+2){
+        arr.push(nums[i+1])
+        arr.push(nums[i])
+    }
+    return arr;
+};
