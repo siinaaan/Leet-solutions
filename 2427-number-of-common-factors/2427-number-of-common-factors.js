13dae38e921979b3aba300962a6ca686e6526600
Muhammed Sinan <siinaaan21@gmail.com>
2026-06-02 11:51:04 +0530

    Time: 1 ms (39.86%), Space: 54.6 MB (18.92%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var commonFactors = function(a, b) {
+  let large = a<b?b:a;
+  let count = 0;
+  for(let i=1;i<=large;i++){
+    if(a%i===0&&b%i===0)count++;
+  }  
+  return count;
+};
