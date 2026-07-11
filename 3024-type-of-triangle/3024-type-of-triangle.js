2665a3587047dc73ab5c1beb0af3d16f2949141a
Muhammed Sinan <siinaaan21@gmail.com>
2026-07-11 09:42:46 +0530

    Time: 0 ms (100%), Space: 55.7 MB (32.2%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var triangleType = function(nums) {
+    if(nums[0]+nums[1]<=nums[2]||
+    nums[1]+nums[2]<=nums[0]||
+    nums[0]+nums[2]<=nums[1]){
+        return "none"
+    }
+    if(nums[0]===nums[1]&&nums[1]===nums[2]){
+        return "equilateral"
+    }else if(nums[0]===nums[1] || nums[1]===nums[2] || nums[0]===nums[2]){
+        return "isosceles"
+    }else{
+        return "scalene"
+    }
+};
