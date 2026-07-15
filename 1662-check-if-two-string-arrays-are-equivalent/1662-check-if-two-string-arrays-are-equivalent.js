3fe05021fa1bbce7ff0a4e8ee091a350167a04f1
Muhammed Sinan <siinaaan21@gmail.com>
2026-07-15 10:31:41 +0530

    Time: 0 ms (100%), Space: 54.3 MB (17.78%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    if(word1.join("")===word2.join("")){
+        return true;
+    }else{
+        return false;
+    }
+};
