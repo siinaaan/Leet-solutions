62843ad5cf40ff428ba96c3ae65bade2d22ff9f5
Muhammed Sinan <siinaaan21@gmail.com>
2026-09-21 09:35:12 +0530

    Time: 0 ms (100%), Space: 54.1 MB (62.78%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+
+        while (!strs[i].startsWith(prefix)) {
+            prefix = prefix.slice(0, prefix.length - 1);
+            if (prefix === "") {
+                return "";
+            }
+        }
+    }
+
+    return prefix;
+};
